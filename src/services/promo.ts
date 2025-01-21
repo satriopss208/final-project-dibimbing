2f2b791c6e238857dce5ae20dac583075a94d245
satriopss208 <satriops20@gmail.com>
2025-01-21 20:08:11 +0700

    add promo service

---
+++ src/services/promo.ts
@@ -0,0 +1,15 @@
+import { fetchAPI } from './api';
+
+export const getPromo = async () => {
+  return await fetchAPI({
+    method: 'GET',
+    url: process.env.NEXT_PUBLIC_BASE_API_URL+`/promos`,
+  });
+};
+
+export const getPromoDetail = async (id: string) => {
+  return await fetchAPI({
+    method: 'GET',
+    url: process.env.NEXT_PUBLIC_BASE_API_URL+`/promos/${id}`,
+  });
+};

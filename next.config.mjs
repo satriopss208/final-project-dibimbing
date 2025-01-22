fb8c68bebb4519e2ba5b4f9fbaf48124c4e8db1a
satriopss208 <satriops20@gmail.com>
2025-01-22 14:33:20 +0700

    add remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
           protocol: 'https',
           hostname: 's-light.tiket.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'www.ancol.com',
+        },
       ],
     },
   };

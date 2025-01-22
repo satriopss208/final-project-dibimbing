0ace1a5bfdbbc14f529c087ee77b422b3cf96543
satriopss208 <satriops20@gmail.com>
2025-01-22 12:14:21 +0700

    add remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
           protocol: 'https',
           hostname: 'travel-journal-api-bootcamp.do.dibimbing.id',
         },
+        {
+          protocol: 'https',
+          hostname: 's-light.tiket.photos',
+        },
       ],
     },
   };

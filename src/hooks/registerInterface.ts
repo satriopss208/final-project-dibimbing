dc8ef28a1d433ee8fe159f783ba56917e7a59ba1
satriopss208 <satriops20@gmail.com>
2025-01-22 17:52:53 +0700

    add register interface

---
+++ src/hooks/registerInterface.ts
@@ -0,0 +1,9 @@
+export interface Register {
+    name: string,
+    email: string,
+    password : string,
+    passwordRepeat : string,
+    role : string,
+    profilePictureUrl : string,
+    phoneNumber : string
+}
